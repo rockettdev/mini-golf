b8aa5d8f9962781be5550e5005219bc3b21ec675
rockettdev <kjr.dev@hotmail.com>
2023-08-21 19:58:28 +0100

    Tailwind Fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import Home from './Components/Home'
 import './App.css';
-
 function App() {
   return (
     <Routes>
---
+++ src/Components/Home/index.js
@@ -0,0 +1,14 @@
+import '../../App.css'
+import '../../index.css'
+
+function Home () {
+    return (
+    <>
+        <div className="flex-auto bg-slate-800">
+            <h1 className="text-4xl text-center"> PutterPal </h1>
+        </div>
+    </>
+    )
+}
+
+export default Home
