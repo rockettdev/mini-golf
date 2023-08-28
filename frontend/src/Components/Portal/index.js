4a6f3d02c02cb3be0070e11a395ce67dabf3c1da
rockettdev <kjr.dev@hotmail.com>
2023-08-28 20:10:09 +0100

    Portal UI updated

--- frontend/src/Components/Portal/index.js
+++ frontend/src/Components/Portal/index.js
@@ -1,14 +1,14 @@
-import '../../App.css'
-
-
 function Portal () {
 
     return (
         <>
             <div className="flex flex-wrap justify-center bg-main-bg bg-cover h-screen">
-
-                <div className="flex-row bg-gray-700 mt-40 w-3/4 h-2/4 rounded-lg">
-                    <div> TEST </div>
+            <h1 className="fixed top-40 text-white font-custom text-4xl"> Welcome ... </h1>
+                <div className="grid grid-cols-2 gap-4 mt-80 w-4/5 h-2/5 rounded-lg text-white text-center font-custom text-lg">
+                    <div className="grid mt-10 bg-gradient-to-tl from-slate-900 to-green-500 rounded-3xl transition duration-300 ease-in-out transform hover:-translate-y-0.5 hover:scale-105 shadow-xl">NEW GAME</div>
+                    <div className="grid mt-10 bg-gradient-to-tl from-slate-900 to-green-500 rounded-3xl transition duration-300 ease-in-out transform hover:-translate-y-0.5 hover:scale-105 shadow-xl">STATS</div>
+                    <div className="grid mb-10 bg-gradient-to-tl from-slate-900 to-green-500 rounded-3xl transition duration-300 ease-in-out transform hover:-translate-y-0.5 hover:scale-105 shadow-xl">LEADERBOARDS</div>
+                    <div className="grid mb-10 bg-gradient-to-tl from-slate-900 to-green-500 rounded-3xl transition duration-300 ease-in-out transform hover:-translate-y-0.5 hover:scale-105 shadow-xl">SETTINGS</div>
                 </div>
         
             </div>
--- frontend/src/Utils/authRoute.js
+++ frontend/src/Utils/authRoute.js
@@ -1,13 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { Navigate, useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function AuthRoute({ element }) {
-  // Replace this with your actual authentication check logic
-
   const location = useLocation()
   const navigate = useNavigate()
-
-
 
   useEffect(() => {
     const token = localStorage.getItem("token")
