b336e83e3c843849cd96132360fd4838db65891f
rockettdev <kjr.dev@hotmail.com>
2023-08-28 23:14:37 +0100

    initialised playerlist inputs

--- frontend/src/Components/Minigame/index.js
+++ frontend/src/Components/Minigame/index.js
@@ -17,18 +17,23 @@ function Minigame () {
 
     return (
         <>
-        {playerNameStatus === false && 
-        <input 
-        value={playerScore.name}
-        onChange={handleNameChange}
-        aria-label="Enter Player Name" 
-        type="name"
-        autoComplete="none" 
-        className="text-sm text-gray-base w-60
-                py-5 px-4 h-2 border border-gray-200 
-                rounded mb-2" />
-        }
-        </>
+        <div className="h-screen flex flex-col 
+                    items-center justify-center bg-main-bg bg-cover">
+            {playerNameStatus === false &&
+            <>
+                <h1 className="font-custom text-3xl text-white">Submit a New Player</h1>
+                <input 
+                value={playerScore.name}
+                onChange={handleNameChange}
+                autoComplete="none" 
+                className="text-sm text-gray-base w-60
+                        py-5 px-4 h-2 border border-gray-200 
+                        rounded mt-12 mb-64" />
+            </>
+            }
+        </div>
+
+                </>
     )
 
 }
