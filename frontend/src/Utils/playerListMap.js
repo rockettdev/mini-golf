a81f290706cecc30bdfe3643b52399396b76544f
rockettdev <kjr.dev@hotmail.com>
2023-08-30 00:32:58 +0100

    conditional page rendering

--- frontend/src/Utils/playerListMap.js
+++ frontend/src/Utils/playerListMap.js
@@ -1,16 +1,26 @@
 import React from 'react'
 
-export default function Playerlistmap({ player }, status) {
+export default function Playerlistmap({ player, status }) {
+
   return (
     <>
-        <div className="grid grid-flow-col">
-            <span className=" w-48 text-left">{player.name}</span>
-            <div>
-              <button className="relative left-6">-</button>
-              <span className=" relative left-6 mx-7">{player.score}</span>
-              <button className="relative left-6">+</button>
-            </div>
+      {status === false &&
+        <div >
+          <span className=" w-48 text-center">{player.name}</span>
         </div>
+      }
+
+      {status === true && 
+        <div className="grid grid-flow-col">
+          <span className=" w-48 text-left">{player.name}</span>
+          <div>
+            <button className="relative left-6">-</button>
+            <span className=" relative left-6 mx-7">{player.score}</span>
+            <button className="relative left-6">+</button>
+          </div>
+        </div>
+    }
+
     </>
   )
 }
